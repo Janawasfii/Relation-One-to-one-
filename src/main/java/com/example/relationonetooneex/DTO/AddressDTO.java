ce93cd6faa0475b273466b3ee22cd44bbdcabd48
Jana Wafi <janawas1@hotmail.com>
2024-08-21 17:25:10 +0300

    Update AddressDTO.java

--- src/main/java/com/example/relationonetooneex/DTO/AddressDTO.java
+++ src/main/java/com/example/relationonetooneex/DTO/AddressDTO.java
@@ -14,15 +14,14 @@ public class AddressDTO {
 
 
     @NotEmpty(message = "Area must not be empty")
-    @Column(columnDefinition = "varchar(50) not null")
     private String area;
 
 
     @NotEmpty(message = "Street must not be empty")
-    @Column(columnDefinition = "varchar(50) not null")
+
     private String street;
 
     @NotNull(message = "Building number must not be null")
-    @Column(columnDefinition = "int not null")
+
     private Integer buildingNumber;
 }
